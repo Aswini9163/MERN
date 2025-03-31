081489f013791b3fbe3c255fe6b1af3671b8cf06
Aswini <aswini.mamidala@gmail.com>
2025-03-31 20:49:39 +0530

    Update dbconfig.js

--- dbconfig/dbconfig.js
+++ dbconfig/dbconfig.js
@@ -1,10 +1,2 @@
 let mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://aswinimamidala3:Iz2W1wnDZ2fPV8Jx@mern9ammarch.h89wtkn.mongodb.net/?retryWrites=true&w=majority&appName=MERN9AMMarch");
-
-/*
-if(con){
-    console.log("connected to database");
-}
-else{
-    console.log("not connected to database");
-}*/
